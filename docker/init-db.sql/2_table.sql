f48710c00f6b9dacd981ab2ab2bc35780beb69dd
lsefiane <lyes.sefiane@gmail.com>
2021-12-21 11:19:28 -0500

    ~ Postgres backup

---
+++ docker/init-db.sql/2_table.sql
@@ -0,0 +1,16 @@
+    CREATE TABLE mitsuke.product(
+        id SERIAL NOT NULL,
+        description VARCHAR (20) NOT NULL,
+        PRIMARY KEY (ID)
+    );
+
+    INSERT INTO mitsuke.product (description) VALUES ('Description-1');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-2');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-3');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-4');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-5');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-6');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-7');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-8');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-9');
+    INSERT INTO mitsuke.product (description) VALUES ('Description-10');
